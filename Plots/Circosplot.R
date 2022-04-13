402ece952cac819b68583caa08c28c58d060cab7
Florian <florian.laubscher@unige.ch>
2022-04-13 13:55:17 +0200

    + Circosplot script

---
+++ Plots/Circosplot.R
@@ -0,0 +1,23 @@
+# Libraries
+library(ggraph)
+library(igraph)
+
+
+# Connection object
+from  <-  match( connect$from, vertices$name)
+to  <-  match( connect$to, vertices$name)
+
+# Plot
+
+ggraph(mygraph, layout = 'dendrogram', circular = TRUE) + 
+  geom_conn_bundle(data = get_con(from = from, to = to), alpha=0.2, width=0.9, aes(colour=..index..)) +
+  scale_edge_colour_distiller(palette = "RdPu") +
+  geom_node_point(aes(filter = leaf, x = x*1.07, y=y*1.07, colour=group, size=value, alpha=0.2)) +
+  scale_size_continuous( range = c(0.1,10) ) +
+  
+  theme_void() +
+  theme(
+    legend.position="none",
+    plot.margin=unit(c(0,0,0,0),"cm"),
+  ) +
+  expand_limits(x = c(-1.3, 1.3), y = c(-1.3, 1.3))
