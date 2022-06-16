01ec1fe809435bca8834477eb0eb74b7a8d5d769
Florian <florian.laubscher@unige.ch>
2022-06-16 16:56:21 +0200

    + script to format downloaded sequence for ORF1 searching

---
+++ src/1line.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#sequence in 1 line and header cut after "."
+for ligne in $(cat $1 | cut -d"." -f 1)
+do
+if [ "${ligne::1}" = ">" ]
+then
+echo ${ligne} >> 1l_$1
+else
+echo -n ${ligne} >> 1l_$1
+fi
+done
+sed -i s"/>/\n>/" 1l_$1
--- src/getFastaFromGB.py
+++ src/getFastaFromGB.py
@@ -10,10 +10,12 @@ record = Entrez.read(handle)
 
 handle.close()
 
-DBaccIDlist=[] #empty for dev
+with open('acc.csv') as f:
+  DBaccIDlist = f.read().splitlines()
+
 for accID in record["IdList"]:
   
-  if accID not in DBaccIDlist:
+  if accID.split(".")[0] not in DBaccIDlist:
     #handle = Entrez.efetch(db="nucleotide", id="AB018887.1", rettype="gb", retmode="text")
     #gb=handle.read()
     #taxo=gb.split("ORGANISM")[1].split("REFERENCE")[0].split(";")[-1].rstrip() #no need for taxa
