f74f202ad2d5ec3c52e4085e465f3bf85726505a
Florian <florian.laubscher@unige.ch>
2022-06-17 11:15:33 +0200

    + script for whole process of updating db

--- src/getFastaFromGB.py
+++ src/getFastaFromGB.py
@@ -4,6 +4,21 @@ from Bio import Entrez
 
 Entrez.email = "####@#####"  # Tell NCBI who you are
 
+# searching term
+#
+# Anelloviridae txid687329
+#
+# Alphatorquevirus txid687331
+#
+# Betatorquevirus txid687332
+#
+# Gammatorquevirus txid687333
+#
+# Hetorquevirus ?????????
+#
+# 'txid687329[Organism:exp] + host="Homo sapiens"'
+#
+
 handle = Entrez.esearch(db="nucleotide",RetMax=1000000, term='txid687329[Organism:exp] + host="Homo sapiens" ', idtype="acc")
 
 record = Entrez.read(handle)
@@ -16,9 +31,10 @@ with open('acc.csv') as f:
 for accID in record["IdList"]:
   
   if accID.split(".")[0] not in DBaccIDlist:
-    #handle = Entrez.efetch(db="nucleotide", id="AB018887.1", rettype="gb", retmode="text")
+    '''if taxa needed'''
+    #handle = Entrez.efetch(db="nucleotide", id=accID, rettype="gb", retmode="text")
     #gb=handle.read()
-    #taxo=gb.split("ORGANISM")[1].split("REFERENCE")[0].split(";")[-1].rstrip() #no need for taxa
+    #taxo=gb.split("ORGANISM")[1].split("REFERENCE")[0].split(";")[-1].rstrip()
     
     handle = Entrez.efetch(db="nucleotide", id=accID, rettype="fasta", retmode="text")
     fasta=handle.read()
