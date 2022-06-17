d80fb227a031675057f687c9e1e970fae2e3be84
Florian <florian.laubscher@unige.ch>
2022-06-17 11:53:58 +0200

    mv code from codon to ORF1

--- src/ORF1.sh
+++ src/ORF1.sh
@@ -192,3 +192,10 @@ FILE=$(echo $1) ;
 
   done
   rm $FILE.temp $FILE.FRAME1.temp $FILE.FRAME2.temp $FILE.FRAME3.temp
+
+#correction header name
+sed -i "s/A_/A/g" ORF_T.fasta
+sed -i "s/T_/T/g" ORF_T.fasta
+sed -i "s/G_/G/g" ORF_T.fasta
+sed -i "s/C_/C/g" ORF_T.fasta
+sed -i "s/N_/N/g" ORF_T.fasta
