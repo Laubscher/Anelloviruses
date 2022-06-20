cddb50149f4e2b76b06e0f114e809b080bcd34b2
Florian <florian.laubscher@unige.ch>
2022-06-20 12:18:03 +0200

    check if exactly 1 sp

--- src/spAttribution.sh
+++ src/spAttribution.sh
@@ -6,4 +6,15 @@ sed -i "s/_ORF1_frAme3_/_/g" output.fa
 
 #TODO reverse too
 
-#Have to check if exactly 1 sp. is tagged (in the header name eg. "_TTV1") and if yes add sequences to the corresponding db and directory
+#Have to . is tagged (in the header name eg. "_TTV1") and if yes add sequences to the corresponding db and directory
+
+for HEADER in $( grep ">" $1); do
+
+  echo $HEADER | tr "_" " " > HEADER_TEMP
+  count=$(cat HEADER_TEMP | sort | uniq | wc -l)
+  if [ $count -eq 2 ] ; then
+  grep -A1 $(echo $HEADER) $1 > OK_sp.fa
+  else ;
+  grep -A1 $(echo $HEADER) $1 > notOK_sp.fa
+  fi
+done
