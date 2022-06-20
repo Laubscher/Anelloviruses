8344bb4f74e59c12c4db6e41efaf8f875e739566
Florian <florian.laubscher@unige.ch>
2022-06-20 15:10:36 +0200

    direct add new ok sequence to fasta db

--- src/spAttribution.sh
+++ src/spAttribution.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
+
+if [ -z "$*" ]; then
+  echo "Arguments are missing"
+  exit 0
+fi
+
+if [ -z "$2" ]; then
+db="db_Alphatorquevirus"
+else
+db=$2
+fi
+
 #orf frame info deleted should not be here
-sed -i "s/_ORF1_frAme1_/_/g" output.fa
-sed -i "s/_ORF1_frAme2_/_/g" output.fa
-sed -i "s/_ORF1_frAme3_/_/g" output.fa
+sed -i "s/_ORF1_frAme1_/_/g" $1
+sed -i "s/_ORF1_frAme2_/_/g" $1
+sed -i "s/_ORF1_frAme3_/_/g" $1
 
 #TODO reverse too
 
@@ -10,11 +22,13 @@ sed -i "s/_ORF1_frAme3_/_/g" output.fa
 
 for HEADER in $( grep ">" $1); do
 
-  echo $HEADER | tr "_" " " > HEADER_TEMP
+  echo $HEADER | tr "_" "\n" > HEADER_TEMP   #/!\ header with "_"
   count=$(cat HEADER_TEMP | sort | uniq | wc -l)
   if [ $count -eq 2 ] ; then
-  grep -A1 $(echo $HEADER) $1 > OK_sp.fa
-  else ;
-  grep -A1 $(echo $HEADER) $1 > notOK_sp.fa
+  sp=$(echo $HEADER | cut -d"_" -f2)
+  ac=$(echo $HEADER | cut -d"_" -f1 | cut -d">" -f2 )
+  grep -A1 $(echo $HEADER) $1 |cut -d"_" -f1 > $db/$sp/$ac.fa
+  else
+  grep -A1 $(echo $HEADER) $1 >> notOK_sp.fa
   fi
 done
