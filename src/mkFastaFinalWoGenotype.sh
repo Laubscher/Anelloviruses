20b769120950c2592fedd662c81e05f04ed0e16d
Florian <florian.laubscher@unige.ch>
2025-02-26 18:44:38 +0100

    refactoring ttmv

--- src/mkFastaFinalWoGenotype.sh
+++ src/mkFastaFinalWoGenotype.sh
@@ -7,15 +7,16 @@ group=$(echo $1 | cut -f2 -d "-")
 ge=$(echo $1 | cut -f1 -d "-")
 for species in $(ls db_$gp); do for fasta in $(ls db_$gp/$species); do for line in $(cat db_$gp/$species/$fasta) ; 
   do 
-  ICTV="False" #defaut
+  ictv="False" #defaut
   if [ "$(grep $species"," lookUpTable/ICTV.csv | cut -d "," -f2)" = "True" ]
-    then ICTV="True"
+    then ictv="True"
+    fi
+  if [ ! "$(echo $fasta | cut -d "." -f2 )" = "fasta" ]
+    then host=$(echo $(grep $(echo $fasta | cut -d "." -f2 )"," lookUpTable/HOST.csv) | cut -d "," -f2)
+    else host="Homo_sapiens"
     fi
   if [ "${line::1}" = ">" ]
-  then if [ "$(echo $fasta | cut -d "-" -f2 )" = "Pt.fasta" ]   
-    then echo $line",GENUS="$ge",GROUP=$group,SPECIES="$species",GENOTYPE=n/a,HOST=Pan_troglodytes,ICTV="$ICTV
-    else echo $line",GENUS="$ge",GROUP=$group,SPECIES="$species",GENOTYPE=n/a,HOST=Homo_sapiens,ICTV="$ICTV    #as defaut Homo_sapiens | ( n/a )
+    then echo $line",GENUS="$ge",GROUP=$group,SPECIES="$species",GENOTYPE=n/a,HOST="$host",ICTV="$ictv
+    else echo $line
     fi
-  else echo $line
-  fi
   done; done; done
